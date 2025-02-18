050ad8c1bc3607b444022d4d4cfd0c8b6ad2b9e8
k0cm8c <Saren_Arterius95@bk.ru>
2025-02-18 13:21:43 +0200

    2nd homework with JS

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -3,3 +3,22 @@
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
+
+  //через оператор if
+
+  let salary = 1000;
+  let grade;
+
+  if (salary >= 1000) {
+    grade = "middle";
+    console.log(grade);
+  } else {
+    grade = "junior";
+    console.log(grade);
+  }
+
+  //либо через тернарное уравнение
+
+  let salary = 1000;
+  let grade = (salary >= 1000) ? "middle" : "junior";
+  console.log(grade);
