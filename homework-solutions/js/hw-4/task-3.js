b65fe4159c7c3336995dfc23854227d22b9767b9
k0cm8c <Saren_Arterius95@bk.ru>
2025-02-18 13:53:46 +0200

    erased unforeseen consequences

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -5,20 +5,4 @@
 Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
   преобразовываясь в number
 
-*/
-
-  let minAge = 18;
-  let maxAge = 60;
-  let age = Number("61");
-
-  if (typeof age !== "number") {
-    console.log("Incorrect data type");
-  } else if (age < minAge) {
-    console.log("You don't have access cause your age is " + age + ". It's less then 18.");
-  } else if (age >= minAge && age < maxAge) {
-    console.log("Welcome  !");
-  } else if (age > maxAge) {
-    console.log("Keep calm and look Culture channel");
-  } else {
-    console.log("Technical work");
-  }
+*//
